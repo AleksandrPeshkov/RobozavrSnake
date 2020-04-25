1dd17292061d3d30f921f9906bd3d1f164f41abf
Aleksandr Peshkov <a.piashkou@gmail.com>
2020-04-25 21:21:09 +0300

    Added axes helper

--- js/main.js
+++ js/main.js
@@ -17,13 +17,18 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
 });
 
-// Adding geometry
+// Adding raycaster
+/**
+ * Raycasting is used for mouse picking (working out what objects in the 3d space the mouse is over) amongst other things.
+ */
+// var raycaster = new THREE.Raycaster();
+// var mouse = new THREE.Vector2();
+
+
+// Adding geometry (snake)
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshLambertMaterial({color: 0x999999});
 var mesh = new THREE.Mesh(geometry, material);
-
-mesh.position.x = 2;
-
 scene.add(mesh);
 
 // Adding light
@@ -31,16 +36,48 @@ var light = new THREE.PointLight(0xffffff, 1, 500);
 light.position.set(10,0,25);
 scene.add(light);
 
+// Adding axes helper
+var axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+
 /**
  * This will create a loop that causes the renderer to draw the scene every time the screen is refreshed (on a typical screen this means 60 times per second). If you're new to writing games in the browser, you might say "why don't we just create a setInterval ?" The thing is - we could, but requestAnimationFrame has a number of advantages.
  */
 function animate() {
-	requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
+  
+  // mesh.rotation.x += 0.01;
+  // mesh.rotation.y += 0.01;
+
 	renderer.render(scene, camera);
 }
+
+// function onMouseMove(event) {
+//   event.preventDefault();
+
+//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+//   mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  
+//   // update the picking ray with the camera and mouse position
+//   raycaster.setFromCamera(mouse, camera);
+  
+//   // calculate objects intersecting the picking ray
+//   var intersects = raycaster.intersectObjects(scene.children, true);
+  
+//   for(var i = 0; i < intersects.length; i++) {
+
+//     this.tl = new TimelineMax();
+//     this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut});
+//     this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut});
+//     this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut});
+//     this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, '=-1.5');
+
+//   }
+// }
+
 animate();
 
-
+// window.addEventListener('mousemove', onMouseMove);
 
 /**
  * Snake 2D logic here ▼
